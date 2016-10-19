3f78d9a2e0f474899c08e95172af14858dfa5c4b
martian206 <martian206@gmail.com>
2016-10-19 13:35:20 +0800

    CH7 - CH8 , crime detail part is done , prepare to do crimeList part

---
+++ Readme.txt
@@ -0,0 +1,10 @@
+start:  CH7 - fragment
+end: ??
+
+
+skills:
+
+1. the using of fragment
+2. the style attribute of XML
+3. adjust layout with graphical interface (p.160)
+4. dateFormat
--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,14 @@ import java.util.UUID;
 public class Crime {
 	private UUID mID;
 	private String mTitle;
+	private Date mDate;
+
+
+	private boolean mSolved;
 
 	public Crime(){
 		mID = UUID.randomUUID();
+		mDate = new Date();
 	}
 
 	public UUID getID() {
@@ -24,5 +30,22 @@ public class Crime {
 
 	public void setTitle(String title) {
 		mTitle = title;
+	}
+
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
 	}
 }
